6965fe3b249000d9d1df02f70dc9a5522e1b65c2
Fabian Wünderich <fabian@wuenderich.de>
2023-09-10 17:41:50 +0200

    Cleanup

--- src/functions.ts
+++
@@ -1,5 +0,0 @@
-import {promisify} from "util";
-
-export const setTimeoutPromise = promisify(setTimeout);
-export const setIntervalPromise = promisify(setInterval);
-export const setImmediatePromise = promisify(setImmediate);
